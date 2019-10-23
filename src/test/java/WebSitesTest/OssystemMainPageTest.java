4b47b428280929f17492cbe35997d2bfc5c213c1
DimaBoyev <dmitriy.boyev@gmail.com>
2019-10-23 17:27:31 +0300

    Test updated

--- src/test/java/WebSitesTest/OssystemMainPageTest.java
+++ src/test/java/WebSitesTest/OssystemMainPageTest.java
@@ -4,6 +4,8 @@ import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import static TestData.TestData.findElement;
+
 
 public class OssystemMainPageTest extends TestData {
 
@@ -17,21 +19,21 @@ public class OssystemMainPageTest extends TestData {
     @Test
     //ESTIMATE PROJECT button, ESTIMATE PROJECT form
     public void testMethod1 () {
-        TestData.waitForElementVisibility("//img[@class='logo']");
-        TestData.waitForElementVisibility("//a[@class='btn'] ").click();
-        TestData.waitForElementVisibility("//form/div[1]/div[1]/input").sendKeys(fullName);
-        TestData.findElement("//form/div[2]/div[1]/input").sendKeys(skype);
-        TestData.findElement("//form/div[3]/div[1]/select").click();
-        TestData.findElement("//form/div[3]/div[1]/select/option[3]").click();
-        TestData.findElement("//form/div[4]/div[1]/textarea").sendKeys(yourMessage);
-        TestData.findElement("//form/div[1]/div[2]/input").sendKeys(email);
-        TestData.findElement("//form/div[2]/div[2]/input").sendKeys(companyName);
-        TestData.findElement("//select[@name='budget']").click();
-        TestData.findElement("//select[@name='budget']/option[5]").click();
-        TestData.findElement("//select[@name='youAre']").click();
-        TestData.findElement("//select[@name='youAre']/option[5]").click();
-        TestData.findElement("//input[@name='submit']").click();
-        String captcha = TestData.waitForElementVisibility("//span[@class='captcha-msg']").getText();
+        waitForElementVisibility("//img[@class='logo']");
+        waitForElementVisibility("//a[@class='btn'] ").click();
+        waitForElementVisibility("//form/div[1]/div[1]/input").sendKeys(fullName);
+        findElement("//form/div[2]/div[1]/input").sendKeys(skype);
+        findElement("//form/div[3]/div[1]/select").click();
+        findElement("//form/div[3]/div[1]/select/option[3]").click();
+        findElement("//form/div[4]/div[1]/textarea").sendKeys(yourMessage);
+        findElement("//form/div[1]/div[2]/input").sendKeys(email);
+        findElement("//form/div[2]/div[2]/input").sendKeys(companyName);
+        findElement("//select[@name='budget']").click();
+        findElement("//select[@name='budget']/option[5]").click();
+        findElement("//select[@name='youAre']").click();
+        findElement("//select[@name='youAre']/option[5]").click();
+        findElement("//input[@name='submit']").click();
+        String captcha = waitForElementVisibility("//span[@class='captcha-msg']").getText();
         Assert.assertEquals("Wrong CAPTCHA", captcha);
         System.out.println("The ESTIMATE PROJECT button clickable, contact form functional");
     }
@@ -39,15 +41,15 @@ public class OssystemMainPageTest extends TestData {
     @Test
     //Contact us button and contact us form
     public void testMethod2 () {
-        TestData.waitForElementVisibility("//a[@id='contact']").click();
-        TestData.waitForElementVisibility("//input[@name='name']").sendKeys(fullName);
-        TestData.findElement("//input[@name='email']").sendKeys(email);
-        TestData.findElement("//div[@class='input-group']/div/button").click();
-        TestData.waitForElementVisibility("//ul/li[5]").click();
-        TestData.findElement("//div[@class='input-group']/input").sendKeys(phone);
-        TestData.findElement("//textarea[@class='message-field']").sendKeys(yourMessage);
-        TestData.findElement("//input[@name='submit']").click();
-        String captcha = TestData.waitForElementVisibility("//span[@class='captcha-msg']").getText();
+        waitForElementVisibility("//a[@id='contact']").click();
+        waitForElementVisibility("//input[@name='name']").sendKeys(fullName);
+        findElement("//input[@name='email']").sendKeys(email);
+        findElement("//div[@class='input-group']/div/button").click();
+        waitForElementVisibility("//ul/li[5]").click();
+        findElement("//div[@class='input-group']/input").sendKeys(phone);
+        findElement("//textarea[@class='message-field']").sendKeys(yourMessage);
+        findElement("//input[@name='submit']").click();
+        String captcha = waitForElementVisibility("//span[@class='captcha-msg']").getText();
         Assert.assertEquals("Wrong Captcha", captcha);
         System.out.println("Contact us button clickable, contact us form functional");
     }
@@ -55,13 +57,13 @@ public class OssystemMainPageTest extends TestData {
     @Test
     //Footer
     public void testMethod3 () {
-        String phoneEuro = TestData.waitForElementVisibility("//div[@class='links-container footer-numbers']/ul/li[1]/a/span").getText();
+        String phoneEuro = waitForElementVisibility("//div[@class='links-container footer-numbers']/ul/li[1]/a/span").getText();
         Assert.assertEquals(phoneEuro, contactPhoneEuro);
-        String phoneUsa = TestData.waitForElementVisibility("//div[@class='links-container footer-numbers']/ul/li[2]/a/span").getText();
+        String phoneUsa = waitForElementVisibility("//div[@class='links-container footer-numbers']/ul/li[2]/a/span").getText();
         Assert.assertEquals(phoneUsa, contactPhoneUsa);
-        String phoneUkraine = TestData.waitForElementVisibility("//div[@class='links-container footer-numbers']/ul/li[3]/a/span").getText();
+        String phoneUkraine = waitForElementVisibility("//div[@class='links-container footer-numbers']/ul/li[3]/a/span").getText();
         Assert.assertEquals(phoneUkraine, contactPhoneUkraine);
-        String email = TestData.waitForElementVisibility("//div[@class='links-container footer-numbers']/ul/li[4]/a/span").getText();
+        String email = waitForElementVisibility("//div[@class='links-container footer-numbers']/ul/li[4]/a/span").getText();
         Assert.assertEquals(email, officeEmail);
         System.out.println("Footer contacts available and correct");
     }
@@ -70,16 +72,16 @@ public class OssystemMainPageTest extends TestData {
     //Services tab
     public void testMethod4 () {
         TestData.waitForElementVisibility("//nav/div/div/div[2]/div[1]/a[1]").click();
-        String section = TestData.waitForElementVisibility("//section[@class='services-header undefined']/div/div/h1").getText();
+        String section = waitForElementVisibility("//section[@class='services-header undefined']/div/div/h1").getText();
         Assert.assertEquals("Services", section);
-        TestData.findElement("//a[@id='contact']").click();
-        TestData.waitForElementVisibility("//input[@name='name']").sendKeys(fullName);
-        TestData.findElement("//input[@name='email']").sendKeys(email);
-        TestData.findElement("//div[@class='input-group']/div/button").click();
-        TestData.waitForElementVisibility("//ul/li[5]").click();
-        TestData.findElement("//div[@class='input-group']/input").sendKeys(phone);
-        TestData.findElement("//textarea[@class='message-field']").sendKeys(yourMessage);
-        TestData.findElement("//input[@name='submit']").click();
+        findElement("//a[@id='contact']").click();
+        waitForElementVisibility("//input[@name='name']").sendKeys(fullName);
+        findElement("//input[@name='email']").sendKeys(email);
+        findElement("//div[@class='input-group']/div/button").click();
+        waitForElementVisibility("//ul/li[5]").click();
+        findElement("//div[@class='input-group']/input").sendKeys(phone);
+        findElement("//textarea[@class='message-field']").sendKeys(yourMessage);
+        findElement("//input[@name='submit']").click();
         String captcha = TestData.waitForElementVisibility("//span[@class='captcha-msg']").getText();
         Assert.assertEquals("Wrong Captcha", captcha);
         System.out.println("Services tab available, Contact us button clickable, contact us form functional");
@@ -91,14 +93,14 @@ public class OssystemMainPageTest extends TestData {
         TestData.waitForElementVisibility("//nav/div/div/div[2]/div[1]/a[2]").click();
         String section = TestData.waitForElementVisibility("//section[@class='portfolio-header-container']/div/div/h1").getText();
         Assert.assertEquals("Portfolio", section);
-        TestData.findElement("//a[@id='contact']").click();
+        findElement("//a[@id='contact']").click();
         TestData.waitForElementVisibility("//input[@name='name']").sendKeys(fullName);
-        TestData.findElement("//input[@name='email']").sendKeys(email);
-        TestData.findElement("//div[@class='input-group']/div/button").click();
+        findElement("//input[@name='email']").sendKeys(email);
+        findElement("//div[@class='input-group']/div/button").click();
         TestData.waitForElementVisibility("//ul/li[5]").click();
-        TestData.findElement("//div[@class='input-group']/input").sendKeys(phone);
-        TestData.findElement("//textarea[@class='message-field']").sendKeys(yourMessage);
-        TestData.findElement("//input[@name='submit']").click();
+        findElement("//div[@class='input-group']/input").sendKeys(phone);
+        findElement("//textarea[@class='message-field']").sendKeys(yourMessage);
+        findElement("//input[@name='submit']").click();
         String captcha = TestData.waitForElementVisibility("//span[@class='captcha-msg']").getText();
         Assert.assertEquals("Wrong Captcha", captcha);
         System.out.println("Portfolio tab available, Contact us button clickable, contact us form functional");
@@ -110,14 +112,14 @@ public class OssystemMainPageTest extends TestData {
         TestData.waitForElementVisibility("//nav/div/div/div[2]/div[1]/a[3]").click();
         String section = TestData.waitForElementVisibility("//section[@class='stack']/span/section/div/div/h1").getText();
         Assert.assertEquals("Technology Partners & Community", section);
-        TestData.findElement("//a[@id='contact']").click();
+        findElement("//a[@id='contact']").click();
         TestData.waitForElementVisibility("//input[@name='name']").sendKeys(fullName);
-        TestData.findElement("//input[@name='email']").sendKeys(email);
-        TestData.findElement("//div[@class='input-group']/div/button").click();
+        findElement("//input[@name='email']").sendKeys(email);
+        findElement("//div[@class='input-group']/div/button").click();
         TestData.waitForElementVisibility("//ul/li[5]").click();
-        TestData.findElement("//div[@class='input-group']/input").sendKeys(phone);
-        TestData.findElement("//textarea[@class='message-field']").sendKeys(yourMessage);
-        TestData.findElement("//input[@name='submit']").click();
+        findElement("//div[@class='input-group']/input").sendKeys(phone);
+        findElement("//textarea[@class='message-field']").sendKeys(yourMessage);
+        findElement("//input[@name='submit']").click();
         String captcha = TestData.waitForElementVisibility("//span[@class='captcha-msg']").getText();
         Assert.assertEquals("Wrong Captcha", captcha);
         System.out.println("Partners tab available, Contact us button clickable, contact us form functional");
@@ -129,14 +131,14 @@ public class OssystemMainPageTest extends TestData {
         TestData.waitForElementVisibility("//nav/div/div/div[2]/div[1]/a[4]").click();
         String section = TestData.waitForElementVisibility("//section[@class='company-about-header']/div/div/h1").getText();
         Assert.assertEquals("About us", section);
-        TestData.findElement("//a[@id='contact']").click();
+        findElement("//a[@id='contact']").click();
         TestData.waitForElementVisibility("//input[@name='name']").sendKeys(fullName);
-        TestData.findElement("//input[@name='email']").sendKeys(email);
-        TestData.findElement("//div[@class='input-group']/div/button").click();
+        findElement("//input[@name='email']").sendKeys(email);
+        findElement("//div[@class='input-group']/div/button").click();
         TestData.waitForElementVisibility("//ul/li[5]").click();
-        TestData.findElement("//div[@class='input-group']/input").sendKeys(phone);
-        TestData.findElement("//textarea[@class='message-field']").sendKeys(yourMessage);
-        TestData.findElement("//input[@name='submit']").click();
+        findElement("//div[@class='input-group']/input").sendKeys(phone);
+        findElement("//textarea[@class='message-field']").sendKeys(yourMessage);
+        findElement("//input[@name='submit']").click();
         String captcha = TestData.waitForElementVisibility("//span[@class='captcha-msg']").getText();
         Assert.assertEquals("Wrong Captcha", captcha);
         System.out.println("Partners tab available, Contact us button clickable, contact us form functional");
